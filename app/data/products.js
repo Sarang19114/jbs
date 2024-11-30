5a40a4f5962ca67f56a118520c5ad7ac0692ad29
Sarang19114 <rastogi.sarang2004@gmail.com>
2024-11-30 18:20:23 +0530

    products sep

---
+++ app/data/products.js
@@ -0,0 +1,18 @@
+export const products = [
+    { thumbnail: "/assets/gallery/G1.jpg" },
+    { thumbnail: "/assets/gallery/G2.jpg" },
+    { thumbnail: "/assets/gallery/G3.jpg" },
+    { thumbnail: "/assets/gallery/G4.jpg" },
+    { thumbnail: "/assets/gallery/G5.jpg" },
+    { thumbnail: "/assets/gallery/G6.jpg" },
+    { thumbnail: "/assets/gallery/G7.jpg" },
+    { thumbnail: "/assets/gallery/G8.jpg" },
+    { thumbnail: "/assets/gallery/G9.jpg" },
+    { thumbnail: "/assets/gallery/G10.jpg" },
+    { thumbnail: "/assets/gallery/G11.jpg" },
+    { thumbnail: "/assets/gallery/G12.jpg" },
+    { thumbnail: "/assets/gallery/G13.jpg" },
+    { thumbnail: "/assets/gallery/G14.jpg" },
+    { thumbnail: "/assets/gallery/G15.jpg" },
+  ];
+  
--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,4 +1,7 @@
+"use client";
+import React, { useEffect, useState } from "react";
 import { HeroParallax } from "@/components/ui/hero-parallax";
+import { products } from "@/app/data/products";
 
 export default function Gallery() {
   return (
@@ -6,54 +9,5 @@ export default function Gallery() {
       <h1 className="text-4xl md:text-5xl font-bold text-center mb-[-100px]">Gallery</h1>
       <HeroParallax products={products} />
     </div>
-  )
+  );
 }
-
-export const products = [
-  {
-    thumbnail: "/assets/gallery/G1.jpg",
-  },
-  {
-    thumbnail: "/assets/gallery/G2.jpg",
-  },
-  {
-    thumbnail: "/assets/gallery/G3.jpg",
-  },
-  {
-    thumbnail: "/assets/gallery/G4.jpg",
-  },
-  {
-    thumbnail: "/assets/gallery/G5.jpg",
-  },
-  {
-    thumbnail: "/assets/gallery/G6.jpg",
-  },
-  {
-    thumbnail: "/assets/gallery/G7.jpg",
-  },
-  {
-    thumbnail: "/assets/gallery/G8.jpg",
-  },
-  {
-    thumbnail: "/assets/gallery/G9.jpg",
-  },
-  {
-    thumbnail: "/assets/gallery/G10.jpg",
-  },
-  {
-    thumbnail: "/assets/gallery/G11.jpg",
-  },
-  {
-    thumbnail: "/assets/gallery/G12.jpg",
-  },
-  {
-    thumbnail: "/assets/gallery/G13.jpg",
-  },
-  {
-    thumbnail: "/assets/gallery/G14.jpg",
-  },
-  {
-    thumbnail: "/assets/gallery/G15.jpg",
-  },
-];
-
